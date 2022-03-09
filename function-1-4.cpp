a82b6f9a5823dba51af0d07b4b7d93e11ceecef2
Pahlavon <a1827303@adelaide.edu.au>
2022-03-09 16:33:24 +1030

    debugged

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -11,7 +11,13 @@ void print_scaled_matrix(int array[3][3], int scale){
     }
     for (int k = 0; k < 3; k++){
         for (int j = 0; j < 3; j++){
+            if (j == 2){
+                cout << arrAY[k][j];
+            }
+            else {
             cout << arrAY[k][j]<<" ";
+            }
+            
         }
         cout <<endl;
     }
