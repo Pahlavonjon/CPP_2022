d8f661fb11362578034adc559d1e40a5670e8a4c
a1827303 <100542387+a1827303@users.noreply.github.com>
2022-03-23 15:13:56 +1030

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 void copy_2d_strings(string first[][2], string second[][2], int n){
-    cout<<"\n";
     for (int h = 0; h < n; h++){
         for (int g = 0; g < 2; g++){
             second[h][g] = first[h][g];
