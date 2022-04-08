f50d089f362fa9b3b76af3a1c740172ad9e4386a
a1827303 <100542387+a1827303@users.noreply.github.com>
2022-04-08 11:48:17 +0930

    Add files via upload

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -9,20 +9,45 @@ int main(){
     cart Meerkat_Cart;
     meerkat M1, M2, M3, M4, M5;
     bool added = true;
-    M1.setName("Meerkat_1");
-    M1.setAge(12);
+    string name1, name2, name3, name4, name5;
+    int age1, age2, age3, age4, age5;
 
-    M2.setName("Meerkat_2");
-    M2.setAge(14);
+    cout <<"\n Enter name 1\n";
+    cin >> name1;
+    cout <<"\n Enter name 2\n";
+    cin >> name2;
+    cout <<"\n Enter name 3\n";
+    cin >> name3;
+    cout <<"\n Enter name 4\n";
+    cin >> name4;
+    cout <<"\n Enter name 5\n";
+    cin >> name5;
 
-    M3.setName("Meerkat_3");
-    M3.setAge(16);
+    cout <<"\n Enter age 1\n";
+    cin >> age1;
+    cout <<"\n Enter age 2\n";
+    cin >> age2;
+    cout <<"\n Enter age 3\n";
+    cin >> age3;
+    cout <<"\n Enter age 4\n";
+    cin >> age4;
+    cout <<"\n Enter age 5\n";
+    cin >> age5;
 
-    M4.setName("Meerkat_4");
-    M4.setAge(18);
+    M1.setName(name1);
+    M1.setAge(age1);
 
-    M5.setName("Meerkat_5");
-    M5.setAge(20);
+    M2.setName(name2);
+    M2.setAge(age2);
+
+    M3.setName(name3);
+    M3.setAge(age3);
+
+    M4.setName(name4);
+    M4.setAge(age4);
+
+    M5.setName(name5);
+    M5.setAge(age5);
 
     added = Meerkat_Cart.addMeerkat(M1);
     added = Meerkat_Cart.addMeerkat(M2);
@@ -34,7 +59,7 @@ int main(){
         cout <<"\n Cart full cannot add!!!\n\n";
     }
 
-    // Meerkat_Cart.printMeerkats();
+    Meerkat_Cart.printMeerkats();
 
     Meerkat_Cart.emptyCart();
 }
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -6,21 +6,36 @@
 using namespace std;
 
 int main(){
+    string CALL_SIGN;
+    cout <<"\n enter call sign:\n";
+    cin >>CALL_SIGN;
     person First_Pilot, First_Co_Pilot, Second_Pilot;
-    aircraft Qantas = aircraft("Call-Sign",First_Pilot,First_Co_Pilot);
+    aircraft Qantas = aircraft(CALL_SIGN,First_Pilot,First_Co_Pilot);
 
-    First_Pilot.setName("Micheal");
+    string PILOT_NAME;
+    string CO_PILOT_NAME;
+    string SECOND_PILOT_NAME;
+
+    cout <<"\n Enter pilot name: \n";
+    cin >> PILOT_NAME;
+    cout <<"\n Enter co-pilot name: \n";
+    cin >> CO_PILOT_NAME;
+
+    First_Pilot.setName(PILOT_NAME);
     Qantas.setPilot(First_Pilot);
-    First_Co_Pilot.setName("George");
+    First_Co_Pilot.setName(CO_PILOT_NAME);
     Qantas.setCoPilot(First_Co_Pilot);
     
 
     cout <<"\n Crew before: \n";
     Qantas.printDetails();
 
-    Second_Pilot.setName("Vincent");
+    cout <<"\n Enter second pilot name: \n";
+    cin >> SECOND_PILOT_NAME;
+
+    Second_Pilot.setName(SECOND_PILOT_NAME);
     Qantas.setPilot(Second_Pilot);
-    First_Pilot.setName("Micheal");
+    First_Pilot.setName(PILOT_NAME);
     Qantas.setCoPilot(First_Pilot);
     
     cout <<"\n Crew after: \n";
